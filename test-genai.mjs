a21ecd9f4a23707b5c4e4044f11b751b4476fc1e
Shree Yadav <shreeyadav0123456789@gmail.com>
2026-08-11 01:08:16 +0530

    Cleanup: Remove old test scripts

--- test-genai.mjs
+++
@@ -1,3 +0,0 @@
-import { GoogleGenAI } from '@google/genai';
-const ai = new GoogleGenAI({ apiKey: 'dummy', baseUrl: 'wss://test.com', httpOptions: { baseUrl: 'wss://test.com' } });
-console.log(Object.keys(ai));
